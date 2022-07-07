fca628f4ae9f00735d9fb01a49960132f21ae648
arpeggi-15 <priyanshu.kr432@gmail.com>
2022-07-07 17:29:56 +0530

    completing advanced section

---
+++ histograms.py
@@ -0,0 +1,49 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+
+img = cv.imread('Photos/cats.jpg')
+cv.imshow('Cats', img)
+
+blank = np.zeros(img.shape[:2],dtype='uint8')
+
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray',gray)
+
+circle = cv.circle(blank, (img.shape[1]//2, img.shape[0]//2), 100, 255, -1)
+
+## Mask for grayscale histogram
+mask = cv.bitwise_and(gray, gray, mask=circle)
+cv.imshow('Mask',mask)
+
+## Mask for color histogram
+masked = cv.bitwise_and(img,img, mask=mask)
+cv.imshow('Mask', masked)
+
+##GrayScale Histogram
+gray_hist = cv.calcHist([gray], [0], mask, [256], [0,256])
+
+plt.figure()
+plt.title('Grayscale Histogram')
+plt.xlabel('Bins')
+plt.ylabel('# of pixels')
+plt.plot(gray_hist)
+plt.xlim([0,256])
+plt.show()
+
+
+##Color Histogram
+plt.figure()
+plt.title('Color Histogram')
+plt.xlabel('Bins')
+plt.ylabel('# of pixels')
+colors = ('b', 'g', 'r')
+for i,col in enumerate(colors):
+    hist = cv.calcHist([img], [i], mask, [256], [0,256])
+    plt.plot(hist, color=col)
+    plt.xlim([0,256])
+
+plt.show()
+
+cv.waitKey(0)
